f75f5ba5e099b81202ca8e66ec8b46194e4e1fa8
Mysticpasta1 <ethana5tc@outlook.com>
2023-12-25 01:49:01 -0600

    change

---
+++ src/main/java/com/mystic/VanillaTestShader.java
@@ -0,0 +1,11 @@
+package com.mystic;
+
+import net.fabricmc.api.ModInitializer;
+
+public class VanillaTestShader implements ModInitializer {
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
--- src/main/java/com/mystic/VanillaTestShaderClient.java
+++ src/main/java/com/mystic/VanillaTestShaderClient.java
@@ -1,8 +1,8 @@
-package com.example;
+package com.mystic;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ExampleModClient implements ClientModInitializer {
+public class VanillaTestShaderClient implements ClientModInitializer {
 	@Override
 	public void onInitializeClient() {
 		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
